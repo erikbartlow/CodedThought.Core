29c563fc12ca0d60b5717d0671ba0f67a06ab181
Erik Bartlow <Ebartlow@versonasystems.com>
2024-12-20 13:29:20 -0600

    Added a new ILogger File Logging Provider.

---
+++ CodedThought.Core/Logging/FileLogger.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+
+using Microsoft.Extensions.Logging;
+
+namespace CodedThought.Core.Logging
+{
+    public class FileLogger : ILogger
+    {
+        private static readonly object _lock = new();
+        protected readonly FileLoggerProvider _provider;
+        protected const string DATEFORMAT = "yyyy-MM-dd HH:mm:ss+00:00";
+        public FileLogger([NotNull] FileLoggerProvider provider)
+        {
+            _provider = provider;
+        }
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
+        public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+                return;
+
+            string fullFilePath = $"{_provider.Options.FolderPath}/{_provider.Options.FileName.Replace("{date}", DateTimeOffset.UtcNow.ToString("yyyyMMdd"))}";
+            string logEntryContent;
+            if (String.IsNullOrEmpty(_provider.Options.LeadingDateFormat) == null)
+                logEntryContent = $"[{DateTimeOffset.UtcNow.ToString(DATEFORMAT)}] {logLevel} {formatter(state, exception)} {(exception != null ? exception.StackTrace : "")}";
+            else
+                logEntryContent = $"{logLevel} {formatter(state, exception)} {(exception != null ? exception.StackTrace : "")}";
+
+            using (StreamWriter streamWriter = new(fullFilePath, true))
+                streamWriter.WriteLine(logEntryContent);
+
+        }
+    }
+}
---
+++ CodedThought.Core/Logging/FileLoggerProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CodedThought.Core.Logging
+{
+    [ProviderAlias("FileLogger")]
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        public readonly FileLoggerOptions Options;
+        public FileLoggerProvider(IOptions<FileLoggerOptions> options)
+        {
+            Options = options.Value;
+            if( !Directory.Exists(Options.FolderPath)) 
+                Directory.CreateDirectory(Options.FolderPath);
+        }
+        public ILogger CreateLogger(string categoryName) => new FileLogger(this);
+        public void Dispose() => GC.SuppressFinalize(this);
+    }
+}
