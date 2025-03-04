057227e362015d7b7a868ee33dce46c9f491ffee
Erik Bartlow <Ebartlow@versonasystems.com>
2025-03-04 10:07:15 -0600

    Added additional properties to the File Logger to allow it manage the number of files and their sizes.

--- CodedThought.Core/Logging/FileLogger.cs
+++ CodedThought.Core/Logging/FileLogger.cs
@@ -20,6 +20,7 @@ namespace CodedThought.Core.Logging
         }
         public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
         public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
         {
             if (!IsEnabled(logLevel))
@@ -27,14 +28,90 @@ namespace CodedThought.Core.Logging
 
             string fullFilePath = $"{_provider.Options.FolderPath}/{_provider.Options.FileName.Replace("{date}", DateTimeOffset.UtcNow.ToString("yyyyMMdd"))}";
             string logEntryContent;
-            if (String.IsNullOrEmpty(_provider.Options.LeadingDateFormat) == null)
+
+            int currentLogFileCount = CountLogFiles(fullFilePath);
+            if (_provider.Options.MaxLogFileCount == 1)
+            {
+            }
+            else
+            {
+                // Delete the oldest file to stay under the max log file count.
+                if (currentLogFileCount > _provider.Options.MaxLogFileCount)
+                    DeleteOldestFile(_provider.Options.FolderPath, _provider.Options.FileName.Replace("{date}", "*"));
+
+                string currentFile = GetCurrentFile(_provider.Options.FolderPath, _provider.Options.FileName.Replace("{data}", "*")) ?? fullFilePath;
+                // If the current file is greater than or equal to the max file size create a new file.
+                if (GetFileSize(currentFile) >= _provider.Options.MaxLogFileSize)
+                {
+                    // Create a new log file if max count is met.
+                    string ext = Path.GetExtension(fullFilePath);
+                    fullFilePath = $"{Path.GetFileNameWithoutExtension(fullFilePath)}_{String.Format("{0:D2}", ++currentLogFileCount)}{ext}";
+                }
+                else
+                {
+                    fullFilePath = currentFile;
+                }
+            }
+            // Write out the content
+            if (!String.IsNullOrEmpty(_provider.Options.LeadingDateFormat) == null)
                 logEntryContent = $"[{DateTimeOffset.UtcNow.ToString(DATEFORMAT)}] {logLevel} {formatter(state, exception)} {(exception != null ? exception.StackTrace : "")}";
             else
                 logEntryContent = $"{logLevel} {formatter(state, exception)} {(exception != null ? exception.StackTrace : "")}";
 
-            using (StreamWriter streamWriter = new(fullFilePath, true))
-                streamWriter.WriteLine(logEntryContent);
+            using StreamWriter streamWriter = new(fullFilePath, true);
+            streamWriter.WriteLine(logEntryContent);
 
+        }
+
+        private int CountLogFiles(string filePath) => Directory.GetFiles(filePath, $"*.{Path.GetExtension(filePath)}").Length;
+
+        private static double GetFileSize(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return 0;
+
+            FileInfo fileInfo = new FileInfo(filePath);
+            return Math.Round(fileInfo.Length / (1024.0 * 1024.0), 2);
+        }
+        private bool CreateNewFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return true;
+            if (GetFileSize(filePath) > Convert.ToDouble(_provider.Options.MaxLogFileSize))
+                return true;
+
+            return false;
+        }
+
+        private static string GetCurrentFile(string directoryPath, string searchPattern)
+        {
+            if (!Directory.Exists(directoryPath))
+                return string.Empty;
+
+            string[] files = Directory.GetFiles(directoryPath, searchPattern);
+
+            FileInfo? newestFile = files
+                .Select(f => new FileInfo(f))
+                .OrderByDescending(f => f.CreationTime)
+                .FirstOrDefault();
+
+            return newestFile != null ? newestFile.FullName : string.Empty;
+        }
+        private static void DeleteOldestFile(string directoryPath, string searchPattern)
+        {
+            if (!Directory.Exists(directoryPath))
+                return;
+
+            string[] files = Directory.GetFiles(directoryPath, searchPattern);
+
+            FileInfo? oldestFile = files
+                .Select(f => new FileInfo(f))
+                .OrderBy(f => f.CreationTime)
+                .FirstOrDefault();
+
+            if (oldestFile != null)
+                File.Delete(oldestFile.FullName);
+            return;
         }
     }
 }
--- CodedThought.Core/Logging/FileLoggerOptions.cs
+++ CodedThought.Core/Logging/FileLoggerOptions.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Logging;
+
 namespace CodedThought.Core.Logging
 {
     public class FileLoggerOptions
@@ -11,5 +13,8 @@ namespace CodedThought.Core.Logging
         public virtual string FileName {  get; set; }
         public virtual string FolderPath {  get; set; }
         public virtual string? LeadingDateFormat {  get; set; }
+        public virtual double? MaxLogFileSize { get; set; }
+        public virtual int? MaxLogFileCount {  get; set; }
+        public virtual LogLevel ConfiguredLogLevel { get; set; }
     }
 }
